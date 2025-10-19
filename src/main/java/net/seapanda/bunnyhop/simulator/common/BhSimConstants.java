f8ff2cfcc7897508f0a2e69b429dbe4826b30a29
SeaPanda <5de6226ufz@gmail.com>
2025-10-19 16:20:34 +0900

    アプリケーションのバージョンを変更した

--- src/main/java/net/seapanda/bunnyhop/simulator/common/BhSimConstants.java
+++ src/main/java/net/seapanda/bunnyhop/simulator/common/BhSimConstants.java
@@ -9,7 +9,7 @@ import net.seapanda.bunnyhop.utility.version.AppVersion;
  */
 public class BhSimConstants {
   /** アプリケーションのバージョン. */
-  public static final AppVersion APP_VERSION = AppVersion.of("bhsim-0.7.0");
+  public static final AppVersion APP_VERSION = AppVersion.of("bhsim-0.7.1");
 
   /** UI 関連のパラメータ. */
   public static class Ui {
