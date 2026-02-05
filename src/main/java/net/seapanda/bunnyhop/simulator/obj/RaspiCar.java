a4068187baed8861743c3a112106132bc9d53c7c
SeaPanda <5de6226ufz@gmail.com>
2026-02-05 22:26:27 +0900

    RaspiCar が静止状態でドリフトする問題を修正した

--- src/main/java/net/seapanda/bunnyhop/simulator/obj/RaspiCar.java
+++ src/main/java/net/seapanda/bunnyhop/simulator/obj/RaspiCar.java
@@ -268,7 +268,7 @@ public class RaspiCar extends PhysicalEntity implements ObjectReflectionProvider
     rigidBody.setSpinningFriction(spinningFriction);
     rigidBody.setRollingFriction(1e-5f);
     rigidBody.setContactStiffnessAndDamping(2000f, 18f);
-    rigidBody.setDamping(0.8f, 0.8f);
+    rigidBody.setDamping(0.9f, 0.9f);
     return rigidBody;
   }
 
